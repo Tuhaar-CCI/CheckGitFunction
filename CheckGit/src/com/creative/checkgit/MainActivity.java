3e0d0781f7c2e44184136182fffd06665987597b
Tushar Vengurlekar <tushar@creativecapsule.com>
2013-11-20 17:16:58 +0530

    Branch 3 Commit 1

--- CheckGit/src/com/creative/checkgit/MainActivity.java
+++ CheckGit/src/com/creative/checkgit/MainActivity.java
@@ -3,6 +3,7 @@ package com.creative.checkgit;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Toast.makeText(MainActivity.this, "Branch 3 Text 1", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
